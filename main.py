da9854cef2da19af95cf1ff46707c6ad4a87fce5
Rahul Aggarwal <rahulagg217@gmail.com>
2026-01-01 19:22:44 +0530

    basic 101

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is my first python program
+
+print("I Like Pizza")
+# variables
+full_name = "Rahul Aggarwal"
+age = 26
+
+gpa = 3.5
+is_student = False
+print(f"Hello {full_name}")
+print(f"my age is {age}")
+print(f"my cgpa is {gpa}")
+print(f"am i a student?: {is_student}")
+
+if is_student:
+    print("I am a student")
+else:
+    print("I am not a student")
+
+friends = 5
+friends += 1
+print(friends)
